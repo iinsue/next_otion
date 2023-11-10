ebcb31e23a968bb6a91b323a6d9ece9eac2f5ced
iinsue <hinsu93@gmail.com>
2023-11-10 20:55:32 +0900

    Heros Component 추가

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,4 +1,5 @@
 import { Heading } from "./_components/heading";
+import { Heroes } from "./_components/heroes";
 
 const MarketingPage = () => {
   return (
@@ -6,6 +7,7 @@ const MarketingPage = () => {
       <div className="min-h-full flex flex-col">
         <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
           <Heading />
+          <Heroes />
         </div>
       </div>
     </>
