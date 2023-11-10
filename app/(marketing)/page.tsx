0d1de7cbfa29a299723b21ce2a033643560bc44d
iinsue <hinsu93@gmail.com>
2023-11-10 20:40:18 +0900

    Heading 만들기

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,15 @@
-export default function Home() {
+import { Heading } from "./_components/heading";
+
+const MarketingPage = () => {
   return (
     <>
-      <div>Hello Notion Clone</div>
+      <div className="min-h-full flex flex-col">
+        <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+          <Heading />
+        </div>
+      </div>
     </>
   );
-}
+};
+
+export default MarketingPage;
