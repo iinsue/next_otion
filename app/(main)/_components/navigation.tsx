e2b2a0c0178824d7c03a4ec6fe219edb7ec9db12
Insu Hong <50567588+iinsue@users.noreply.github.com>
2023-11-17 11:24:15 +0900

    만들어진 Note 의 Title 을 Sidebar 에 표시

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,15 +1,21 @@
 "use client";
 
-import { cn } from "@/lib/utils";
-import { ChevronsLeft, MenuIcon } from "lucide-react";
-import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
+import { usePathname } from "next/navigation";
 import { useMediaQuery } from "usehooks-ts";
+import { useQuery } from "convex/react";
+
+import { cn } from "@/lib/utils";
+import { api } from "@/convex/_generated/api";
+
 import { UserItem } from "./user-item";
+
+import { ChevronsLeft, MenuIcon } from "lucide-react";
 
 export const Navigation = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
+  const documents = useQuery(api.documents.get);
 
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -118,7 +124,9 @@ export const Navigation = () => {
           <UserItem />
         </div>
         <div className="mt-4">
-          <p>Documents</p>
+          {documents?.map((document) => (
+            <p key={document._id}>{document.title}</p>
+          ))}
         </div>
         <div
           onMouseDown={handleMouseDown}
