e93c9281d99f45e2f8c775b84a9e96d66a81a267
Insu Hong <50567588+iinsue@users.noreply.github.com>
2023-11-17 19:47:30 +0900

    페이지 삭제 기능 중간 저장

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -29,6 +29,7 @@ import {
   Settings,
   Trash,
 } from "lucide-react";
+import { TrashBox } from "./trash-box";
 
 export const Navigation = () => {
   const pathname = usePathname();
@@ -165,7 +166,7 @@ export const Navigation = () => {
               className="p-0 w-72"
               side={isMobile ? "bottom" : "right"}
             >
-              <p>Trash box</p>
+              <TrashBox />
             </PopoverContent>
           </Popover>
         </div>
