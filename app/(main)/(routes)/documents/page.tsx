9ddcc8e243f5e41d859e90d4e3d7f09ad1da0e49
Insu Hong <50567588+iinsue@users.noreply.github.com>
2023-11-17 10:00:02 +0900

    Add Documents Page Image & Button

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,8 +1,36 @@
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+
+import { PlusCircle } from "lucide-react";
+
 const DocumentsPage = () => {
+  const { user } = useUser();
+
   return (
     <>
       <div className="h-full flex flex-col items-center justify-center space-y-4">
-        This is a protected DocumentsPage
+        <Image
+          src="/empty.png"
+          width="300"
+          height="300"
+          alt="Empty"
+          className="dark:hidden"
+        />
+        <Image
+          src="/empty-dark.png"
+          width="300"
+          height="300"
+          alt="Empty"
+          className="hidden dark:block"
+        />
+        <h2>Welcome to {user?.firstName}&apos;s Jotion</h2>
+        <Button>
+          <PlusCircle className="h-4 w-4 mr-2" />
+          Create a note
+        </Button>
       </div>
     </>
   );
