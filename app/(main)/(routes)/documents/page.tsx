d7e260e8f6b31bb5c79f8a699518d6129c916877
Insu Hong <50567588+iinsue@users.noreply.github.com>
2023-11-17 11:12:07 +0900

    Add Create a note API

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -3,11 +3,26 @@
 import Image from "next/image";
 import { useUser } from "@clerk/clerk-react";
 import { Button } from "@/components/ui/button";
+import { useMutation } from "convex/react";
+import { toast } from "sonner";
+
+import { api } from "@/convex/_generated/api";
 
 import { PlusCircle } from "lucide-react";
 
 const DocumentsPage = () => {
   const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = create({ title: "Untitled" });
+
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created!",
+      error: "Failed to create a new note.",
+    });
+  };
 
   return (
     <>
@@ -27,7 +42,7 @@ const DocumentsPage = () => {
           className="hidden dark:block"
         />
         <h2>Welcome to {user?.firstName}&apos;s Jotion</h2>
-        <Button>
+        <Button onClick={onCreate}>
           <PlusCircle className="h-4 w-4 mr-2" />
           Create a note
         </Button>
